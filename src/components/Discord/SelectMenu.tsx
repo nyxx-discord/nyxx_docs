51d94408549e00e81100477ce4ac260747008458
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2022-03-24 14:52:09 +0100

    Add select menus and buttons for components.mdx

---
+++ src/components/Discord/SelectMenu.tsx
@@ -0,0 +1,82 @@
+import {
+  DiscordButton,
+  DiscordButtonProps,
+  DiscordButtons,
+  DiscordDefaultOptions,
+  DiscordInteraction,
+  DiscordMarkdown,
+  DiscordMessage,
+  DiscordMessages,
+  DiscordOptionsContext,
+  _DiscordDefaultOptions,
+} from "@discord-message-components/react";
+import useIsBrowser from "@docusaurus/useIsBrowser";
+import useInterval from "@site/src/hooks/useInterval";
+import * as React from "react";
+import DiscordMultiSelect from "./DiscordMultiSelect";
+
+const options: _DiscordDefaultOptions = {
+  ...DiscordDefaultOptions,
+  profiles: {
+    l7ssha: {
+      author: "l7ssha",
+      avatar: "https://i.imgur.com/KZmUYEt.png",
+      roleColor: "#5865f2",
+    },
+    mycoolbot: {
+      author: "My Cool Bot",
+      avatar: "red",
+      roleColor: "rgb(235, 69, 158)",
+      bot: true,
+    },
+  },
+};
+
+export default function Component({
+  lightTheme,
+  ephemeral,
+  commandContent,
+  content,
+  menuOptions,
+  buttonTypes,
+  urls,
+  disabled,
+  children,
+}: {
+  lightTheme?: boolean;
+  ephemeral?: boolean;
+  commandContent?: string;
+  content?: string;
+  menuOptions?: string[];
+  buttonTypes?: DiscordButtonProps["type"][];
+  urls?: string[];
+  disabled?: boolean;
+  children?: React.ReactNode;
+}) {
+  const browser = useIsBrowser();
+  lightTheme ??= browser ? localStorage.getItem("theme") === "light" : false;
+  const [light, setLight] = React.useState(lightTheme);
+  useInterval(() => {
+    setLight(browser ? localStorage.getItem("theme") === "light" : false);
+  });
+  return (
+    <DiscordOptionsContext.Provider value={options}>
+      <DiscordMessages lightTheme={light}>
+        <DiscordMessage profile="mycoolbot">
+          <DiscordInteraction
+            slot="interactions"
+            ephemeral={!!ephemeral}
+            command={true}
+            profile="l7ssha"
+          >
+            {commandContent}
+          </DiscordInteraction>
+          {content}
+          <DiscordMultiSelect disabled={!!disabled} lightTheme={light} options={menuOptions}>
+            {children}
+          </DiscordMultiSelect>
+        </DiscordMessage>
+      </DiscordMessages>
+    </DiscordOptionsContext.Provider>
+  );
+}
---
+++ src/components/Discord/components.tsx
@@ -0,0 +1,88 @@
+import {
+  DiscordButton,
+  DiscordButtonProps,
+  DiscordButtons,
+  DiscordDefaultOptions,
+  DiscordInteraction,
+  DiscordMarkdown,
+  DiscordMessage,
+  DiscordMessages,
+  DiscordOptionsContext,
+  _DiscordDefaultOptions,
+} from "@discord-message-components/react";
+import useIsBrowser from "@docusaurus/useIsBrowser";
+import useInterval from "@site/src/hooks/useInterval";
+import * as React from "react";
+
+const options: _DiscordDefaultOptions = {
+  ...DiscordDefaultOptions,
+  profiles: {
+    l7ssha: {
+      author: "l7ssha",
+      avatar: "https://i.imgur.com/KZmUYEt.png",
+      roleColor: "#5865f2",
+    },
+    mycoolbot: {
+      author: "My Cool Bot",
+      avatar: "red",
+      roleColor: "rgb(235, 69, 158)",
+      bot: true,
+    },
+  },
+};
+
+export default function Component({
+  lightTheme,
+  ephemeral,
+  commandContent,
+  content,
+  buttonsContent,
+  buttonTypes,
+  urls,
+  disabled,
+}: {
+  lightTheme?: boolean;
+  ephemeral?: boolean;
+  commandContent?: string;
+  content?: string;
+  buttonsContent?: string[];
+  buttonTypes?: DiscordButtonProps["type"][];
+  urls?: string[];
+  disabled: boolean[];
+}) {
+  const browser = useIsBrowser();
+  lightTheme ??= browser ? localStorage.getItem("theme") === "light" : false;
+  const [light, setLight] = React.useState(lightTheme);
+  useInterval(() => {
+    setLight(browser ? localStorage.getItem("theme") === "light" : false);
+  });
+  return (
+    <DiscordOptionsContext.Provider value={options}>
+      <DiscordMessages lightTheme={light}>
+        <DiscordMessage profile="mycoolbot">
+          <DiscordInteraction
+            slot="interactions"
+            ephemeral={!!ephemeral}
+            command={true}
+            profile="l7ssha"
+          >
+            {commandContent}
+          </DiscordInteraction>
+          {content}
+          <DiscordButtons>
+            {buttonsContent &&
+              buttonsContent.map((buttonContent, index) => (
+                <DiscordButton disabled={disabled && disabled[index]}
+                  key={index}
+                  type={buttonTypes && (buttonTypes[index] as any)}
+                  url={urls && urls[index]}
+                >
+                  {buttonContent}
+                </DiscordButton>
+              ))}
+          </DiscordButtons>
+        </DiscordMessage>
+      </DiscordMessages>
+    </DiscordOptionsContext.Provider>
+  );
+}
