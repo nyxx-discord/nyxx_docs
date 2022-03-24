07ab93c182395d8189af238df788858dddf2a990
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2022-03-24 14:51:44 +0100

    Create Chevron for SelectMenu

---
+++ src/components/Discord/OutboundMultiSelectChevron.tsx
@@ -0,0 +1,21 @@
+import React, { ReactElement } from "react";
+
+export default function ({ rotate }: { rotate?: boolean }): ReactElement {
+  return (
+    <span className={`outbound-chevron-icon ${!!rotate ? "rotate" : ""}`}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true"
+        focusable="false"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill="currentColor"
+          d="M16.59 8.59003L12 13.17L7.41 8.59003L6 10L12 16L18 10L16.59 8.59003Z"
+        ></path>
+      </svg>
+    </span>
+  );
+}
