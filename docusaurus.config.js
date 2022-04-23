3f8ba8f5e6a165e52256c0e78cec9fad1ff49307
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2022-04-23 19:08:58 +0200

    Add search bar, also disable blog, add (placeholder?) logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,17 +1,42 @@
 // @ts-check
 
+/**
+ * @typedef DocusaurusSearchLocalOptions
+ * @property {boolean} [indexDocs=true] Whether to index docs.
+ * @property {boolean} [indexBlog=true] Whether to index blog.
+ * @property {boolean} [indexPages=false] Whether to index pages.
+ * @property {string|string[]} [docsRouteBasePath='/docs'] Base route path(s) of docs. Slash at beginning is not required.
+ * @property {string|string[]} [blogRouteBasePath='/blog'] Base route path(s) of blog. Slash at beginning is not required.
+ * @property {string|string[]} [language='en'] All [lunr-languages](https://github.com/MihaiValentin/lunr-languages) supported languages.
+ * @property {boolean} [hashed=false] Whether to add a hashed query when fetching index (based on the content hash of all indexed `*.md` in {@link DocusaurusSearchLocalOptions.docsDir `docsDir`} and {@link DocusaurusSearchLocalOptions.blogDir `blogDir`} if applicable).
+ * @property {string|string[]} [docsDir='docs'] The dir(s) of docs to get the content hash, it's relative to the dir of your project.
+ * @property {string|string[]} [blogDir='blog'] Just like the {@link DocusaurusSearchLocalOptions.docsDir `docsDir`} but applied to blog.
+ * @property {boolean} [removeDefaultStopWordFilter=false] Sometimes people (E.g., us) want to keep the English stop words as indexed, since they maybe are relevant in programming docs.
+ * @property {boolean} [highlightSearchTermsOnTargetPage=false] Highlight search terms on target page.
+ * @property {number} [searchResultLimits=8] Limit the search results.
+ * @property {number} [searchResultContextMaxLength=50] Set the max length of characters of each search result to show.
+ * @property {{[key: string]: string}} [translations] Set translations of this theme.
+ * @property {string|RegExp|(string | RegExp)[]} [ignoreFiles] Set the match rules to ignore some files.
+ */
+
 const lightCodeTheme = require('prism-react-renderer/themes/vsLight');
 const darkCodeTheme = require('prism-react-renderer/themes/vsDark');
 const creationYear = 2016;
-const dev = process.env.NODE_ENV === 'dev';
 const url = dev ? process.env.DEV_URL : process.env.PROD_URL;
 // Note to self, urls should'nt be finished with a slash (/)
+const url = dev
 const {
-  REPO_NAME_DEV: repoDev,
   BRANCH_NAME_DEV: branchDev,
   REPO_NAME_PROD: repoProd,
   REPO_BRANCH_NAME: branchProd,
 } = process.env;
+
+/** @type {DocusaurusSearchLocalOptions & { id?: string; }} */
+const docusaurusSearchLocalOptions = {
+  hashed: true,
+  language: ['en'],
+  indexBlog: false,
+};
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -24,6 +49,14 @@ const config = {
   favicon: 'img/favicon.ico',
   organizationName: 'nyxx-discord',
   projectName: 'nyxx',
+  deploymentBranch: 'main',
+
+  plugins: [
+    [
+      require.resolve('@easyops-cn/docusaurus-search-local'),
+      docusaurusSearchLocalOptions,
+    ],
+  ],
 
   presets: [
     [
@@ -40,6 +73,8 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/global.css'),
         },
+
+        blog: false,
       }),
     ],
   ],
@@ -62,10 +97,10 @@ const config = {
         },
       ],
       navbar: {
-        title: 'Nyxx Docs',
+        title: 'nyxx Docs',
         logo: {
           alt: 'Nyxx Logo',
-          src: 'img/Nyxx_Logo.svg',
+          src: 'img/Nyxx_Logo@3.svg',
         },
         items: [
           {
@@ -92,8 +127,12 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
+                label: 'Getting Started',
                 to: '/docs/intro',
+              },
+              {
+                label: 'Pub Docs',
+                href: 'https://pub.dev/documentation/nyxx/latest',
               },
             ],
           },
@@ -112,6 +151,18 @@ const config = {
               {
                 label: 'GitHub',
                 href: 'https://github.com/nyxx-discord/nyxx',
+              },
+              {
+                label: 'nyxx_interactions',
+                href: 'https://github.com/nyxx-discord/nyxx_interactions',
+              },
+              {
+                label: 'nyxx_commands',
+                href: 'https://github.com/nyxx-discord/nyxx_commands',
+              },
+              {
+                label: 'nyxx_sharding',
+                href: 'https://github.com/nyxx-discord/nyxx_sharding',
               },
             ],
           },
